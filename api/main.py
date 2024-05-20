978beb0a57e7615732a1b5e5d1b3b87fafa0aa94
alex.lin <alex@mac.local>
2024-05-20 20:45:44 +0800

    Fix: logging config

--- api/main.py
+++ api/main.py
@@ -6,22 +6,24 @@ from .db import model
 from .db.database import engine
 from .router import account
 from .utility.exception_handler import ExceptionHandlerMiddleware
-import logging
+import logging, sys
 
-app = FastAPI(title="Technical Assessment")
+app = FastAPI(title="Login Authentication")
 
 # create database table
 model.Base.metadata.create_all(bind=engine)
 
 # logging config
-logger = logging.Logger(__name__)
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s %(levelname)s: %(message)s"
-)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+log_formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setFormatter(log_formatter)
+logger.addHandler(stream_handler)
 
 
 @app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
+def validation_exception_handler(request: Request, exc: RequestValidationError):
     responses = []
     errors = exc.errors()
     for error in errors:
@@ -37,6 +39,16 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
         status_code=status.HTTP_400_BAD_REQUEST,
         content=jsonable_encoder({"success": False, "reason": str(responses)}),
     )
+
+
+@app.middleware("http")
+async def logger_middleware(request: Request, call_next):
+    client = request.client
+    method = request.method
+    url = request.url
+    logger.info(f"client: {client}, method: {method}, url: {url}")
+    response = await call_next(request)
+    return response
 
 
 app.add_middleware(ExceptionHandlerMiddleware)
